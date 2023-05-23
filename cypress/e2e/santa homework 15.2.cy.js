43520fb12a4189415e672e71f476a4c6cb91a46f
NikitaBaranov1992 <2486010@mail.ru>
2023-05-24 00:18:51 +0500

    homework 15.1.3

---
+++ cypress/e2e/santa homework 15.2.cy.js
@@ -0,0 +1,33 @@
+describe("santa", () => {
+  beforeEach(() => {
+    cy.visit("https://staging.lpitko.ru/login");
+    cy.get("[name=email]").type("4932843@gmail.com");
+    cy.get("[name=password]").type("GoodPassword10");
+    cy.get(".form-auth__button").click();
+  });
+
+  it("account", () => {
+    cy.contains("Никита Баранов").click({ force: true });
+    cy.location("pathname").should("eq", "/account");
+    cy.go("back");
+  });
+
+  it("boxes", () => {
+    cy.contains("Коробки").click({ force: true });
+    cy.location("pathname").should("eq", "/account/boxes");
+    cy.go("back");
+  });
+
+  it("randomizer", () => {
+    cy.contains("Быстрая жеребьевка").click();
+    cy.location("pathname").should("eq", "/randomizer");
+    cy.go("back");
+  });
+
+  it("add box", () => {
+    cy.contains("Создать коробку").click();
+    //cy.get(".txt-secondary txt txt--grey").should("have.length", 1);
+    cy.location("pathname").should("eq", "/box/new");
+    cy.go("back");
+  });
+});
