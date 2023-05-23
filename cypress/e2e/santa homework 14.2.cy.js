2f4b5ea7efa0f997657a51c30691fe4bbeb70580
NikitaBaranov1992 <2486010@mail.ru>
2023-05-24 00:18:57 +0500

    homework 15.1.3

--- cypress/e2e/santa homework 14.2.cy.js
+++ cypress/e2e/santa homework 14.2.cy.js
@@ -1,4 +1,4 @@
-describe("santa open", () => {
+/*describe("santa open", () => {
   beforeEach(() => {
     cy.visit("https://staging.lpitko.ru");
   });
@@ -6,4 +6,4 @@ describe("santa open", () => {
   it.only("santa open is correct", () => {
     cy.get(".home-page__logo").should("have.length", 1);
   });
-});
+});*/
